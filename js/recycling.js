ffb3f0ec3f92430d136ce38b7e8c45a8695e4efc
gemmaelliott <44118774+gemmaelliott@users.noreply.github.com>
2018-12-05 19:40:12 +0000

    adding tags to map

--- js/recycling.js
+++ js/recycling.js
@@ -1,15 +1,55 @@
-<script>
-// Initialize and add the map
+
+
 function initMap() {
-  // The location of Uluru
-  var uluru = {lat: 55.837914, lng: -4.246540};
-  // The map, centered at Uluru
-  var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 12, center: uluru});
-  // The marker, positioned at Uluru
-  var marker = new google.maps.Marker({position: uluru, map: map});
+
+	var broadway = {
+		info: '<a href="http://www.weeescotland.co.uk/" target="_blank"><strong>WEEE Scotland</strong></a><br>\
+					249 Govan Rd,<br> Glasgow G51 1HJ',
+		lat: 55.857762,
+		long: -4.288380
+	};
+
+	var belmont = {
+		info: '<a href="http://recycle-glasgow.co.uk/" target="_blank"><strong>Recycle Glasgow</strong></a><br>\
+					707 London Rd,<br> Glasgow G40 3AS',
+		lat: 55.851839,
+		long: -4.221314
+	};
+
+  var sheridan = {
+    info: '<a href="https://www.glasgow.gov.uk/index.aspx?articleid=15893" target="_blank"><strong>Recycling Centre</strong></a><br>\
+          425 Polmadie Rd,<br> Glasgow G42 0PJ',
+    lat: 55.835206,
+    long: -4.240147
+  };
+
+	var locations = [
+      [broadway.info, broadway.lat, broadway.long, 0],
+      [belmont.info, belmont.lat, belmont.long, 1],
+      [sheridan.info, sheridan.lat, sheridan.long, 2],
+    ];
+
+	var map = new google.maps.Map(document.getElementById('map'), {
+		zoom: 13,
+		center: new google.maps.LatLng(55.850727, -4.257595),
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	});
+
+	var infowindow = new google.maps.InfoWindow({});
+
+	var marker, i;
+
+	for (i = 0; i < locations.length; i++) {
+		marker = new google.maps.Marker({
+			position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+			map: map
+		});
+
+		google.maps.event.addListener(marker, 'click', (function (marker, i) {
+			return function () {
+				infowindow.setContent(locations[i][0]);
+				infowindow.open(map, marker);
+			}
+		})(marker, i));
+	}
 }
-    </script>
-    <script async defer
-    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBa-x9TainBhx5VH8ivtp1UVodtRSsyKe8&callback=initMap">
-    </script>
