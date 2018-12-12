0d40626441d034457cdf009ffc48051b087e0bc3
Gemma <gambroomhead@gmail.com>
2018-12-12 18:25:38 +0000

    Updated the html and csss files for the Get Involved pages. Final iteration.

---
+++ getinvolved_javas.js
@@ -0,0 +1,35 @@
+//Start of Charity Expanding Grid.
+function openTab(tabName) {
+  var i, x;
+  x = document.getElementsByClassName("containerTab");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  document.getElementById(tabName).style.display = "block";
+}
+
+//Start of Popup chat box.
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+
+// Start of Footnote REFERENCE drop down.
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+//End of Footnote regerence drop down.
